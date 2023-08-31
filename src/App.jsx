ae0cd5461255302c1327dd0b749c0c0ba7bfe10a
Matthew7991 <Matthew7991@gmx.de>
2023-08-31 16:47:33 +0200

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={<h1>Hello</h1>}
+        />
+      </Routes>
+    </>
+  )
+}
+
+export default App
